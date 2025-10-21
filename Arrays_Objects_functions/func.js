7c936c8db039cb5325a08532a01329fd8dc089db
Rubini <rubinibalusamy@gmail.com>
2025-10-21 15:08:18 +0530

    update arrays&functions

---
+++ Arrays_Objects_functions/func.js
@@ -0,0 +1,85 @@
+const add=()=>{
+var a=10;
+var b=20
+var c=a+b;
+console.log(c)
+}
+add()
+const sub=()=>{
+var d=30;
+var e=20
+var f=d-e;
+console.log(f)
+}
+sub()
+const mul=()=>{
+var number1=3
+var number2=10
+var number3=number1*number2;
+console.log(number3)
+}
+mul()
+const div=()=>{
+var divident=155
+var divider=10
+var quotient=divident/divider
+console.log(quotient)
+}
+div()
+const avg=()=>{
+var n1=3
+var n2=10
+var n3=23
+var n4=55
+var n5=n1+n2+n3+n4/4;
+console.log(n5)
+}
+avg()
+const sum=()=>{
+var sum=0
+for(let i=1;i<=10;i++)
+sum= sum+i;
+console.log(sum)
+}
+sum()
+const invite=()=>{
+    console.log("Welcome to our home")
+}
+invite()
+const greater=()=>{
+    var a=10
+    var b=20
+    if(a>b){
+        console.log("a is greater");
+    }
+    else{
+        console.log("b is greater");
+    }
+}
+greater()
+const smaller=()=>{
+    var a=10
+    var b=20
+    if(a<b){
+        console.log("a is smaller");
+    }
+    else{
+        console.log("b is smaller");
+    }
+}
+smaller()
+const intro=()=>{
+    console.log("My name is Rubini")
+}
+intro()
+var y=20
+const sub1=()=>{
+console.log(y-1);
+
+}
+sub1()
+var x=10
+const add1=()=>{
+console.log(x+1);
+}
+add1()
